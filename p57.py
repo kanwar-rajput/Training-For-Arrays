95cda801801eca88a346adf7363366cf8580e86f
Kanwar Adnan <52542755+kanwar-rajput@users.noreply.github.com>
2022-03-03 20:44:04 +0500

    Tells whether Two Arrays have same elements

---
+++ p57.py
@@ -0,0 +1,26 @@
+def isEqual(list1 , list2):
+    """
+        Tells whether Two Arrays have same elements
+    """
+    if (len(list1) != len(list2)):
+        return False
+    else:
+        for i in range(len(list1)):
+            if list1[i] != list2[i]:
+                return False
+    return True
+
+# Arrays
+l1 = [1,2,3,4]
+l2 = [2,3,4,5]
+l3 = [1,2,3,4]
+l4 = [1,2,3]
+
+# Test Case 1
+print(isEqual(l1,l2))
+
+# Test Case 2
+print(isEqual(l1,l3))
+
+# Test Case 3
+print(isEqual(l1,l4))
