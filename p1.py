baf3c3a05042acbb79e57e405d186d084e62a3be
Kanwar Adnan <52542755+kanwar-rajput@users.noreply.github.com>
2022-03-03 17:37:22 +0500

    Prints the elements of Array.

---
+++ p1.py
@@ -0,0 +1,13 @@
+def printArray(list):
+    """
+        Function to print the elements of array.
+    """
+    for i in list:
+        print(i,end=" ")
+
+# Declaring and Initializing List / Array
+l1 = [1,2,3,4]
+
+# Calling the function
+print("Printing Element of Array")
+printArray(l1) # 1 2 3 4
